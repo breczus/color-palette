1df40a64fdba5b63c0de45c94d4f5c3fc4beb595
breczus <breczus@gmail.com>
2022-01-07 22:47:39 +0100

    Add colorHelpers.js to generate colors in shades

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Pallete from "./Pallete";
 import seedColor from "./seedColor";
+import generatePalette from "./colorHelpers";
 
 function App() {
+  console.log(generatePalette(seedColor[4]))
   return (
     <div>
       <Pallete {...seedColor[4]} />
