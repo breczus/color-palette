4faaefa7ed5d74345acbce197871017ddbcfaf97
breczus <breczus@gmail.com>
2022-01-12 17:27:53 +0100

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Palette from './Palette';
+import { Route, Routes } from 'react-router-dom';
 import seedColor from './seedColor';
 import generatePalette from './colorHelpers';
 
 function App() {
 	return (
-		<div>
-			<Palette palette={generatePalette(seedColor[4])} />
-		</div>
+		<Routes>
+			<Route exact path='/' element={<h1>Palette List</h1>} />
+			<Route exact path='/palette/:id' element={<h1>1palette</h1>} />
+		</Routes>
+
+		// <div>
+		// 	<Palette palette={generatePalette(seedColor[4])} />
+		// </div>
 	);
 }
 
