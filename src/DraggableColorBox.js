69c170526935c1eac90e0842f63d9a67f7418bdb
breczus <breczus@gmail.com>
2022-02-01 23:32:25 +0100

    move styles out and create constans.js

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -2,35 +2,7 @@ import React from 'react';
 import { withStyles } from '@material-ui/styles';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { SortableElement } from 'react-sortable-hoc';
-
-const styles = {
-	root: {
-		width: '20%',
-		height: '25%',
-		margin: '0 auto',
-		display: 'inline-block',
-		position: 'relative',
-		cursor: 'pointer',
-		marginBottom: '-3.5px',
-		'&:hover svg': {
-			color: 'white',
-			transform: 'scale(1.5)',
-		},
-	},
-	boxContent: {
-		position: 'absolute',
-		width: '100%',
-		left: '0px',
-		bottom: '0px',
-		padding: '10px',
-		color: 'black',
-		letterSpacing: '1px',
-		textTransform: 'uppercase',
-		fontSize: '12px',
-		display: 'flex',
-		justifyContent: 'space-between',
-	},
-};
+import styles from './styles/DraggableColorBox';
 
 const DraggableColorBox = SortableElement((props) => {
 	return (
